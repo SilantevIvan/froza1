8016ea3fbf6f6b35df40a4b33557a9d9017a9fdb
SilantevIvan <silantev.ivan.7@gmail.com>
2023-08-25 14:33:58 +0200

    Добавление роутеров, добавление папочки дата

--- src/App.js
+++ src/App.js
@@ -1,17 +1,60 @@
-import React, { useEffect } from "react";
-import { BrowserRouter, Routs, Rout } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routs, Rout, Route, Routes } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
+import { Navbar, Footer, Sidebar, TheemeSettings } from "./components";
+import { Orders, Products, Home, Contacts, ECommerce } from "./pages";
 import "./App.css";
 
 const App = () => {
+  const activeMenu = true;
   return (
     <div>
       <BrowserRouter>
-        <div className="flex relative bg-white dark:bg-main-dark-bg">
+        <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
-            <TooltipComponent></TooltipComponent>
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type="button"
+                className="text-3xl p-3 hover:drop-shadow-xl 
+                hover:bg-light-gray text-white"
+                style={{ background: "blackgray", borderRadius: "50%" }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+              <Sidebar />
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              <Sidebar />
+            </div>
+          )}
+          <div
+            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+              activeMenu ? "md:ml-72" : "flex-2"
+            }`}
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+              <Navbar />
+            </div>
+          </div>
+
+          <div>
+            <Routes>
+              {/* Dashboard */}
+              <Route path="/" element={<ECommerce />} />
+              <Route path="/ecommerce" element={<ECommerce />} />
+              {/* Pages */}
+              <Route path="/home" element={<Home />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/contacts" element={<Orders />} />
+            </Routes>
           </div>
         </div>
       </BrowserRouter>
---
+++ src/pages/index.jsx
@@ -0,0 +1,5 @@
+export { default as Orders } from "./Orders";
+export { default as Home } from "./Home";
+export { default as Products } from "./Products";
+export { default as ECommerce } from "./ECommerce";
+export { default as Contacts } from "./Contacts";
